011ff3668507aa4f57c5466baccc4da15f31fd38
Abdullah Hussain <malikhussain412990@gmail.com>
2024-08-10 21:15:28 +0500

    Create code.py

---
+++ python/python-tutorial/code.py
@@ -0,0 +1,9 @@
+name = input("What is your name? :")
+age = input("What is your age? :")
+classes = input("In which class does you read? :")
+
+print(f"Hello {name}")
+print(f"you are {age} years old")
+print(f"you read in class {classes}")
+#code completed
+# Time to run our program 
